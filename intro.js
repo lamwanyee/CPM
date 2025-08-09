8aa3ad60d3b00a9330ed991474793633ab10ba25
ChooHuiHarn <166193576+ChooHuiHarn@users.noreply.github.com>
2025-08-09 20:36:48 +0800

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,29 @@
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display='flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display='none'
+}
+
+
+let darkmode= localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkMode= () =>{
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode','active')
+}
+
+const disableDarkMode= () =>{
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode','null')
+}
+
+if(darkmode=== "active") enableDarkMode()
+
+themeSwitch.addEventListener("click",() =>{
+  darkmode= localStorage.getItem('darkmode')
+  darkmode !=="active"? enableDarkMode(): disableDarkMode() 
+})
